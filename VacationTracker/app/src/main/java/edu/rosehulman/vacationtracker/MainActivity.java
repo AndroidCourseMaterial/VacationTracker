aa92c9adea5f306281a7754be96ec78a566cc2e7
Matt Boutell <boutell@gmail.com>
2017-02-06 11:41:42 -0500

    Add maps and change to Terre Haute

--- VacationTracker/app/src/main/java/edu/rosehulman/vacationtracker/MainActivity.java
+++ VacationTracker/app/src/main/java/edu/rosehulman/vacationtracker/MainActivity.java
@@ -14,10 +14,17 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ToggleButton;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class
-MainActivity extends AppCompatActivity {
+MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +32,30 @@ MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
 
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng terreHaute = new LatLng(39.465542, -87.375924);
+
+        mMap.addMarker(new MarkerOptions().position(terreHaute).title("Terre Haute"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(terreHaute, 5.0f));
     }
 
     @Override
